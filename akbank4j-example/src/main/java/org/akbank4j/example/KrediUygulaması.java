e30cb2d2e026bef55624699434a2b05b577fc5a7
Onur Arslan <onur.arslan@mebitech.com>
2017-01-10 22:04:55 +0200

    tüketici, mortgage ve araç kredisi için örnekler tamamlandı

--- akbank4j-example/src/main/java/org/akbank4j/example/KrediUygulaması.java
+++ akbank4j-example/src/main/java/org/akbank4j/example/KrediUygulaması.java
@@ -24,6 +24,7 @@ public class KrediUygulaması {
     CreditInterestRatesModel tuketiciKredisi = akbank.getCreditInterestRate(
             AkbankParameters.CreditInterestRates.CONSUMER_LOAN).getData();
 
+    System.out.println("TÜKETİCİ KREDİSİ");
     System.out.println("Kredi Başlangıç Tarihi: " + tuketiciKredisi.getApplicationStartDate());
     System.out.println("Kredi Bitiş Tarihi: " + tuketiciKredisi.getApplicationEndDate());
     System.out.println("En Düşük Kredi Dönemi: " + tuketiciKredisi.getMinTerm());
@@ -38,6 +39,54 @@ public class KrediUygulaması {
     System.out.println("Kredi Tutarı: " + tuketiciKredisi.getCreditAmount());
     System.out.println("Kredi Faizleri");
     for (CreditInterestRecord kredi : tuketiciKredisi.getInterestArrayList()) {
+      System.out.println("En Düşük Dönem: " + kredi.getMinTerm());
+      System.out.println("En Yüksek Dönem: " + kredi.getMaxTerm());
+      System.out.println("Aralık: " + kredi.getRate());
+      System.out.println("");
+    }
+
+    CreditInterestRatesModel mortgageKredi = akbank.getCreditInterestRate(
+            AkbankParameters.CreditInterestRates.MORTGAGE_LOAN).getData();
+
+    System.out.println("MORTGAGE KREDİSİ");
+    System.out.println("Kredi Başlangıç Tarihi: " + mortgageKredi.getApplicationStartDate());
+    System.out.println("Kredi Bitiş Tarihi: " + mortgageKredi.getApplicationEndDate());
+    System.out.println("En Düşük Kredi Dönemi: " + mortgageKredi.getMinTerm());
+    System.out.println("En YÜksek Kredi Dönemi: " + mortgageKredi.getMaxTerm());
+    System.out.println("En Düşük Kredi Miktarı: " + mortgageKredi.getMinAmount());
+    System.out.println("En Yüksek Kredi Miktarı: " + mortgageKredi.getMaxAmount());
+    System.out.println("Kredi Oranı: " + mortgageKredi.getTaxRate());
+    System.out.println("Fon Oranı " + mortgageKredi.getFundRate());
+    System.out.println("Masraf Miktarı: " + mortgageKredi.getExpenseAmount());
+    System.out.println("Faiz Dönemi: " + mortgageKredi.getInterestTerm());
+    System.out.println("Faiz Sayısı: " + mortgageKredi.getInterestCount());
+    System.out.println("Kredi Tutarı: " + mortgageKredi.getCreditAmount());
+    System.out.println("Kredi Faizleri");
+    for (CreditInterestRecord kredi : mortgageKredi.getInterestArrayList()) {
+      System.out.println("En Düşük Dönem: " + kredi.getMinTerm());
+      System.out.println("En Yüksek Dönem: " + kredi.getMaxTerm());
+      System.out.println("Aralık: " + kredi.getRate());
+      System.out.println("");
+    }
+
+    CreditInterestRatesModel tasitKredisi = akbank.getCreditInterestRate(
+            AkbankParameters.CreditInterestRates.CAR_LOAN).getData();
+
+    System.out.println("TAŞIT KREDİSİ");
+    System.out.println("Kredi Başlangıç Tarihi: " + tasitKredisi.getApplicationStartDate());
+    System.out.println("Kredi Bitiş Tarihi: " + tasitKredisi.getApplicationEndDate());
+    System.out.println("En Düşük Kredi Dönemi: " + tasitKredisi.getMinTerm());
+    System.out.println("En YÜksek Kredi Dönemi: " + tasitKredisi.getMaxTerm());
+    System.out.println("En Düşük Kredi Miktarı: " + tasitKredisi.getMinAmount());
+    System.out.println("En Yüksek Kredi Miktarı: " + tasitKredisi.getMaxAmount());
+    System.out.println("Kredi Oranı: " + tasitKredisi.getTaxRate());
+    System.out.println("Fon Oranı " + tasitKredisi.getFundRate());
+    System.out.println("Masraf Miktarı: " + tasitKredisi.getExpenseAmount());
+    System.out.println("Faiz Dönemi: " + tasitKredisi.getInterestTerm());
+    System.out.println("Faiz Sayısı: " + tasitKredisi.getInterestCount());
+    System.out.println("Kredi Tutarı: " + tasitKredisi.getCreditAmount());
+    System.out.println("Kredi Faizleri");
+    for (CreditInterestRecord kredi : tasitKredisi.getInterestArrayList()) {
       System.out.println("En Düşük Dönem: " + kredi.getMinTerm());
       System.out.println("En Yüksek Dönem: " + kredi.getMaxTerm());
       System.out.println("Aralık: " + kredi.getRate());
